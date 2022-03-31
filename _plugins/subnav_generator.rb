f24aa3c1fa30923d1b89d98a83c07b6ba495777c
Vitalii Elenhaupt <velenhaupt@gmail.com>
2022-03-31 21:23:04 +0300

    Improve generator

--- _plugins/subnav_generator.rb
+++ _plugins/subnav_generator.rb
@@ -2,11 +2,13 @@
 
 require 'nokogiri'
 
-class MySubnavGenerator < Jekyll::Generator
+class SubnavGenerator < Jekyll::Generator
   def generate(site)
     parser = Jekyll::Converters::Markdown.new(site.config)
 
     site.pages.each do |page|
+      next unless page.ext == '.md'
+
       doc = Nokogiri::HTML(parser.convert(page['content']))
       page.data['subnav'] = []
       doc.css('h2').each do |heading|
